14db826b32340d526baab01249c07c1966ee0c71
Alexis <alexis.geiger@etu.u-bordeaux.fr>
2022-12-24 17:09:56 +0100

    making the (theorical) spritestack, not tested yet

--- spritestack.cpp
+++ spritestack.cpp
@@ -4,15 +4,23 @@
  *** devcomm 1 : the number of slice, the width and the height of the slice ***
  *** must be declared manually for now ***/
 
-void Engine::spritestack()
+void Engine::spritestack(Sprite (&slice)[])
 {
-	m_Texture.loadFromFile("Graphics/Hunter.png");
-	int sliceNumber = 16;
-	int sliceWidth = 16;
-	int sliceHeight = 16;
+	m_Texture.loadFromFile("Graphics/Hunter.png"); // Here to use graphics in your depository 
+	int sliceNumber = 16; // Manually putting the number of slices
+	int sliceWidth = 16; // This will help to set just a square of the spritesheet
+	int sliceHeight = 16; // This too
 
-/* TO DO : 
- * For loop to get the real sprite stacking : having first a sprite already declared ? 
- * what should be returned ? Nothing ? To test.
- * should the texture be in the parameters instead ? */  
+	int yAxis = 0; // can be replaced by xAxis if the spritesheet is in the horizontal position. 
+
+/* Sprite array
+ * For loop with each sprite part being a slice
+ * TO DO : the position of said sprite stack */
+
+	for(int i = 0; i < sliceNumber; i++)
+	{
+		slice[i].setTexture(m_Texture);
+		slice[i].setTextureRect(sf::IntRect(0, yAxis, sliceWidth, sliceHeight));
+		yAxis += sliceHeight;
+	}
 }
